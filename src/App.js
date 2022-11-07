c15c963d25f7c9cb45554800294d9b947f11da17
DESKTOP-JDH895S\Hilmatrix <hilmaul@gmail.com>
2022-11-07 18:08:06 +0700

    modifikasi tampilan React App

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="Hilmatrix">
+        <header className="Hilmatrix">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
+          <h1 className="App-title">Welcome from Buffcatman !</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <img src={buffcatman-circle} className="App-logo-reverse" alt="logo" />
         </p>
       </div>
     );
