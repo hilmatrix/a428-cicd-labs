e2fe11125fc57c7b22ce0083c0cc9b036b935b2c
DESKTOP-JDH895S\Hilmatrix <hilmaul@gmail.com>
2022-11-07 18:17:42 +0700

    perbaikan kode react lagi

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="Hilmatrix">
-        <header className="Hilmatrix">
+      <div className="App">
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome from Buffcatman !</h1>
         </header>
